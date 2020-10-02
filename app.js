b5e7bea1b9c829990c5d3f85e83daae716189a38
sylvestre <sylvestre.marie@gmail.com>
2020-10-02 14:43:50 +0200

    CRUD Sauce #1

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ mongoose.connect('mongodb+srv://Marie_Sylvestre:CapucinE1*@cluster0.q2lux.mongod
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
+const Sauces = require('./models/sauces');
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -18,25 +20,37 @@ app.use((req, res, next) => {
 app.use(bodyParser.json());
 
 app.post('/api/sauces', (req, res, next) => {
-    const sauces = [
-        {
-            _id: 'oeihfzeoi',
-            title: 'Mon premier objet',
-            description: 'Les infos de mon premier objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 4900,
-            userId: 'qsomihvqios',
-        },
-        {
-            _id: 'oeihfzeomoihi',
-            title: 'Mon deuxième objet',
-            description: 'Les infos de mon deuxième objet',
-            imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-            price: 2900,
-            userId: 'qsomihvqios',
-        },
-    ];
-    res.status(200).json(sauces);
+    delete req.body._id;
+    const sauces = new Sauces({
+        ...req.body
+    });
+    sauces.save()
+        .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.get('/api/sauces', (req, res, next) => {
+    Sauces.findOne({ _id: req.params.id })
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(404).json({ error }));
+});
+
+app.get('/api/sauces/:id', (req, res, next) => {
+    Sauces.findOne({ _id: req.params.id })
+        .then(sauces => res.status(200).json(sauces))
+        .catch(error => res.status(404).json({ error }));
+});
+
+app.put('/api/sauces/:id', (req, res, next) => {
+    Sauces.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+        .catch(error => res.status(400).json({ error }));
+});
+
+app.delete('/api/sauces/:id', (req, res, next) => {
+    Sauces.deleteOne({ _id: req.params.id })
+        .then(() => res.status(200).json({ message: 'Objet supprimé !'}))
+        .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
