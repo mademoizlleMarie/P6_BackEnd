d40ba32cbfd22bce9d2522b9a3fc3efa8856d52a
sylvestre <sylvestre.marie@gmail.com>
2020-10-18 16:41:20 +0200

    gestion des likes et dislikes #5

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -2,7 +2,6 @@ const Sauces = require('../models/sauce');
 const fs = require('fs');
 
 exports.createSauce = (req, res, next) => {
-    console.log(req.body);
     const sauceToCreate = JSON.parse(req.body.sauce);
     delete sauceToCreate._id;
     const saucesRequest = new Sauces({
@@ -60,6 +59,70 @@ exports.getAllSauces =  (req, res, next) => {
         }
     ).catch(
         error => res.status(400).json({error: error})
+    );
+};
+
+exports.likeOrDislikeSauce =  (req, res, next) => {
+
+    const userId = req.body.userId;
+    const choiseLike = req.body.like;
+    const sauceId = req.params.id;
+    console.log("Id user  : "+ userId)
+    console.log("Choix like dislike : "+ choiseLike)
+    console.log("id Sauce : "+ sauceId)
+
+
+   Sauces.findOne({
+       _id: sauceId,
+   }).then(
+       sauce => {
+           if (choiseLike === 1) {
+
+               sauce.usersLiked.push(userId)
+               Sauces.updateOne(
+                   {_id: sauceId},
+                   {
+                       usersLiked: sauce.usersLiked,
+                       likes: sauce.likes+1
+                   }
+               )
+                   .then(() => res.status(200).json({message: 'Vous aimez cette sauce'}))
+                   .catch(error => res.status(400).json({error}));
+           }
+
+           if (choiseLike === 0) {
+               if(sauce.usersLiked.find(user => user == userId) != undefined ){
+                   Sauces.updateOne(
+                       {_id: sauceId},
+                       {
+                           usersLiked: sauce.usersLiked.filter(user => user != userId),
+                           likes: sauce.likes-1
+                       }
+                   )
+                       .then(() => res.status(200).json({message: 'Vous n\' aimez pas cette sauce'}))
+                       .catch(error => res.status(400).json({error}));
+               } else {
+                   Sauces.updateOne({_id: sauceId}, {usersDisliked: sauce.usersDisliked.filter(user => user != userId), dislikes: sauce.dislikes-1})
+                       .then(() => res.status(200).json({message: 'Vous n\' aimez pas cette sauce'}))
+                       .catch(error => res.status(400).json({error}));
+               }
+           }
+
+           if (choiseLike === -1) {
+               sauce.usersDisliked.push(userId)
+               Sauces.updateOne(
+                   {_id: sauceId},
+                   {
+                       usersDisliked: sauce.usersDisliked,
+                       dislikes: sauce.dislikes+1
+                   }
+               )
+                   .then(() => res.status(200).json({message: 'Vous aimez cette sauce'}))
+                   .catch(error => res.status(400).json({error}));
+           }
+       }
+    ).catch(
+        error => res.status(404).json({error: error})
     );
 };
 
